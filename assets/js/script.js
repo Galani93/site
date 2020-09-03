64c7f326f4bf45f7cbc13c2a9e55632caf02e18f
Lubos Rendek <web@linuxconfig.org>
2020-09-03 14:32:06 +0400

    Պետական օգտատիրոջ կառուցվածքի սահմանում ( script for button )

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,28 @@
 
 
 $( document ).ready(function() {
-    $("#toggle-itmes .dropdown-item").on('click', function(event) {
+    let $elements = $("#toggle-itmes .dropdown-item");
+    $elements.on('click', function(event) {
 
+        // clouse all;
+        $elements.each(function( index ) {
+            $(this).children("div").addClass("hidden");
+        });
+
+        // toogle  this
         $(this).children("div").toggleClass("hidden");
-
         // stop clouse
         event.stopPropagation();
+    });
+
+
+    $("#toggle-itmes .toogle-block").on('click', function(event) {
+        console.log(event.target);
+        var target = $( event.target );
+        if(!target.is("botton")){
+            event.stopPropagation();
+        }
+        // stop clouse
     });
 
 });
