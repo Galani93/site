7932a30cce2979b2f0e4964887ee88bc1124dc0c
Galani93 <ani_galstyan_93@mail.ru>
2020-10-19 16:54:47 +0400

    3․1․2․Կազմակերպում/Մեկ անձ/Կազմակերպել նոր գնման գործընթաց (bug) 03

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,6 +30,58 @@ $( document ).ready(function() {
 
 });
 
-$( "#v-pills-profile-tab" ).click(function() {
-    $( "#v-pills-02" ).show( "slow" );
-});
+
+!function (t) {
+    "use strict";
+    var e = function () {
+    };
+    e.prototype.init = function () {
+        t("#sa-basic").on("click", function () {
+            Swal.fire("Any fool can use a computer")
+        }), t("#sa-title").click(function () {
+            Swal.fire("The Internet?", "That thing is still around?", "question")
+        }), t("#sa-success").click(function () {
+            Swal.fire("", "Ձեր պատվերն ընդունված է", "success")
+        })
+    }, t.SweetAlert = new e, t.SweetAlert.Constructor = e
+}(window.jQuery), function (t) {
+    "use strict";
+    window.jQuery.SweetAlert.init()
+}();
+
+var timer;
+
+var compareDate = new Date();
+compareDate.setDate(compareDate.getDate() + 3); //just for this demo today + 3 days
+
+timer = setInterval(function() {
+    timeBetweenDates(compareDate);
+}, 1000);
+
+function timeBetweenDates(toDate) {
+    var dateEntered = toDate;
+    var now = new Date();
+    var difference = dateEntered.getTime() - now.getTime();
+
+    if (difference <= 0) {
+
+        // Timer done
+        clearInterval(timer);
+
+    } else {
+
+        var seconds = Math.floor(difference / 1000);
+        var minutes = Math.floor(seconds / 60);
+        var hours = Math.floor(minutes / 60);
+        var days = Math.floor(hours / 24);
+
+        hours %= 24;
+        minutes %= 60;
+        seconds %= 60;
+
+        $("#days").text(days);
+        $("#hours").text(hours);
+        $("#minutes").text(minutes);
+        $("#seconds").text(seconds);
+    }
+}
